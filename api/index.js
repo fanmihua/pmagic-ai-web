c728154dbc627a2cea3d39496e57442e7cccaca9
fan18660557495 <fan18660557495@gmail.com>
2026-07-01 15:18:56 +0800

    Use single Vercel API entrypoint

--- api/index.js
+++ api/index.js
@@ -1,1 +1,15 @@
-export { default, config } from "./_handler.js";
+import { getApp } from "../server/dist/src/app.js";
+
+let appPromise;
+
+export default async function handler(request, response) {
+  appPromise ||= getApp();
+  const app = await appPromise;
+  app.server.emit("request", request, response);
+}
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
